7473d91ab454d9b5abbb3cd66d3b2631722aebfd
Gabriel Silva <gabrielmdhtt@gmail.com>
2021-04-21 21:31:16 -0400

    first stable version

--- snowdevil/Controllers/ProductController.cs
+++ snowdevil/Controllers/ProductController.cs
@@ -1,18 +1,31 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using snowdevil.Models;
+using snowdevil.Models.ViewModels;
 
 namespace snowdevil.Controllers
 {
     public class ProductController : Controller
     {
-        private IProductRepository productRepository;
+        private IProductRepository productsRepository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repository)
         {
-            productRepository = repository;
+            productsRepository = repository;
         }
 
-        public ViewResult List() => View(productRepository.Products);
+        public ViewResult List(string category, int page = 1) => View(new ProductsListViewModel
+        {
+            Products = productsRepository.Products.Where(p => category == null || p.Category == category).OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = category == null ? productsRepository.Products.Count() : productsRepository.Products.Where(e => e.Category == category).Count()
+            },
+            CurrentCategory = category
+        });
     }
 }
--- snowdevil/Startup.cs
+++ snowdevil/Startup.cs
@@ -8,18 +8,30 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using snowdevil.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace snowdevil
 {
     public class Startup
     {
+        IConfigurationRoot Configuration; 
+        public Startup(IWebHostEnvironment env) { 
+            Configuration = new ConfigurationBuilder().SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json").Build(); 
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
 
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))); 
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddMvc(options => options.EnableEndpointRouting = false).AddNewtonsoftJson(); 
+            services.AddMemoryCache(); services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,11 +40,38 @@ namespace snowdevil
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes =>
             {
+
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Product}/{action=List}/{id?}");
+                    name: null, 
+                    template: "{category}/Page{page:int}", 
+                    defaults: new { controller = "Product", action = "List" }
+                    ); 
+
+                routes.MapRoute(
+                    name: null,
+                    template: "Page{page:int}", 
+                    defaults: new { controller = "Product", action = "List", page = 1 }
+                    ); 
+                
+                routes.MapRoute(
+                    name: null, 
+                    template: "{category}", 
+                    defaults: new { controller = "Product", action = "List", page = 1 }
+                    );
+                
+                routes.MapRoute(
+                    name: null, 
+                    template: "", 
+                    defaults: new { controller = "Product", action = "List", page = 1 }
+                    ); 
+                
+                routes.MapRoute(
+                    name: null, 
+                    template: "{controller}/{action}/{id?}"
+                    );
             });
         }
     }
